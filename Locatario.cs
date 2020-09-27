e918f15a37addd6886c53808d11f39c464a36758
Andre Soares <andresoares37@gmail.com>
2020-09-27 17:02:04 -0300

    first commit

---
+++ Locatario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores3
+{
+    public class Locatario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Locatario(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email; ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vetores3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Locatario[] vect = new Locatario[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+
+
+                Console.WriteLine("Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Locatario(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
